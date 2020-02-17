1eb32a521e7aeeb6734e800e8db10b35998b60e5
benowino7 <bensonowino7@gmail.com>
2020-02-17 11:22:02 +0300

    created general configuration class with different functions

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kebaso:steve254@localhost/blog2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool@localhost/blogs'
     UPLOADED_PHOTOS_DEST ='app/static'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # email configurations
@@ -32,7 +32,7 @@ class TestConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kebaso:steve254@localhost/blog2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool@localhost/blogs'
 
 class DevConfig(Config):
     '''
@@ -41,7 +41,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kebaso:steve254@localhost/blog2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool@localhost/blogs'
 
     DEBUG = True
     ENV = 'development'
