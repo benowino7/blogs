3f875eb4cf40b70424248d154175be8f778c14b1
benowino7 <bensonowino7@gmail.com>
2020-02-17 11:13:36 +0300

    email authentication  function

--- app/routes.py
+++ app/routes.py
@@ -204,6 +204,6 @@ def reset_token(token):
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user.password = hashed_password
         db.session.commit()
-        flash('password has been updated! You are now able to log in', 'success')
+        flash('password has been updated! Please log in', 'success')
         return redirect(url_for('login'))
     return render_template('reset_token.html', title='Reset Password', form=form)
