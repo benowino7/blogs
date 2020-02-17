ec04fa0ebfbfb6e88988a8dd828c9830b2c92c46
benowino7 <bensonowino7@gmail.com>
2020-02-17 15:30:41 +0300

    edited the request file for quotes

--- app/requests.py
+++ app/requests.py
@@ -1,13 +1,12 @@
-# import requests
-# from .models import Quote
+from .models import Quote
 
-# url = "http://quotes.stormconsultancy.co.uk/random.json"
+url = "http://quotes.stormconsultancy.co.uk/random.json"
 
-# def get_quote():
-#     """
-#     Function to receive http request and return an instance of Quote class
-#     """
-#     response = requests.get(url).json()
+def get_quote():
+    """
+    Function to receive http request and return an instance of Quote class
+    """
+    response = get(url).json()
 
-#     random_quote = Quote(response.get("author"), response.get("quote"))
-#     return random_quote
+    random_quote = Quote(get("author"),get("quote"))
+    return random_quote
