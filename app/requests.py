fe6422edbba66a8c343dfc9461394326a93037cc
benowino7 <bensonowino7@gmail.com>
2020-02-18 15:36:12 +0300

    deploymen erro sorting

--- app/requests.py
+++ app/requests.py
@@ -1,12 +1,13 @@
-# from .models import Quote
+import requests
+from .models import Quote
 
-# url = "http://quotes.stormconsultancy.co.uk/random.json"
+url = "http://quotes.stormconsultancy.co.uk/random.json"
 
-# def get_quote():
-#     """
-#     Function to receive http request and return an instance of Quote class
-#     """
-#     response =(url).json()
+def get_quote():
+    """
+    Function to consume http request and return a Quote class instance
+    """
+    response = requests.get(url).json()
 
-#     random_quote = Quote(get("author"),("quote"))
-#     return random_quote
+    random_quote = Quote(response.get("author"), response.get("quote"))
+    return random_quote
